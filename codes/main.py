b2ca37533f11f27259723a92793231536739388f
Sonal Singh <sonal.singh221999@gmail.com>
2021-08-01 09:49:29 +0530

    Tinker Gui is added

--- codes/main.py
+++ codes/main.py
@@ -26,7 +26,8 @@ def get_covid_data():
 
 # Parsing individual country's data from worldmeter.
 def get_country_data():
-    url = "https://www.worldometers.info/coronavirus/country/"
+    name = textfield.get()
+    url = "https://www.worldometers.info/coronavirus/country/"+name
     html_data = get_html_data(url)
     bs = bs4.BeautifulSoup(html_data.text,'html.parser')
     info_div = bs.find("div",class_="content-inner").findAll("div", id="maincounter-wrap")
@@ -37,4 +38,32 @@ def get_country_data():
         count = info_div[block].find("span", class_ = None).get_text()
         all_data = all_data + text + " " + count +"\n" 
 
+
+# Reloading the world's data.
+def reload():
+    new_data = get_covid_data()
+    mainlable['text']=new_data
+
+
 get_covid_data() 
+
+
+# Adding tkinter gui to the above code.
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Covid Tracker")
+f = ("poppins",25,"bold")
+
+mainlable = tk.Label(root,text=get_covid_data(), font=f)
+mainlable.pack()
+
+textfield = tk.Entry(root,width=50,font=f)
+textfield.pack(padx=10,pady=10)
+
+gbtn = tk.Button(root, text="Get Data", font=f, relief="solid", command=get_country_data)
+gbtn.pack(pady=10)
+
+rbtn = tk.Button(root, text="Reload", font=f, relief="solid", command=reload)
+rbtn.pack()
+
+root.mainloop()
